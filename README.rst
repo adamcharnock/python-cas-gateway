6d87c171f2d593a65b1a7e81d7c371bc2af9fd04
Adam Charnock <adam@adamcharnock.com>
2017-12-17 01:24:24 +0000

    Adding arguments

--- README.rst
+++ README.rst
@@ -9,6 +9,41 @@ it will sit between your reverse proxy and your software.
 
 This is a proof of concept and the proxying logic is naive.
 
+Use
+---
+
+.. code-block:: plain
+
+    $ cas_gateway -h
+    usage: cas_gateway [-h] [--redis-url REDIS_URL] [--timeout TIMEOUT]
+                       [--bind-host BIND_HOST] [--bind-port BIND_PORT]
+                       [--cas-version CAS_VERSION] [--chunk-size CHUNK_SIZE]
+                       --backend-url BACKEND_URL --cas-url CAS_URL
+
+    Python CAS Gateway
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      --redis-url REDIS_URL
+                            Redis URL (default: redis://127.0.0.1:6379/0)
+      --timeout TIMEOUT     Request timeout (default: 10)
+      --bind-host BIND_HOST
+                            Address to bind to (default: 0.0.0.0)
+      --bind-port BIND_PORT
+                            Port to bind to (default: 8000)
+      --cas-version CAS_VERSION
+                            CAS version in use (default: 3)
+      --chunk-size CHUNK_SIZE
+                            Chunk size for streaming responses back to the client
+                            (default: 4096)
+
+    required named arguments:
+      --backend-url BACKEND_URL
+                            URL to the backend to be proxied (e.g.
+                            http://myhost.com:8888) (default: None)
+      --cas-url CAS_URL     URL to the CAS server (default: None)
+
+
 Docker
 ------
 
--- python_cas_gateway/gateway.py
+++ python_cas_gateway/gateway.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 
 import aiohttp
@@ -10,18 +11,17 @@ from aioredis import create_pool
 from yarl import URL
 
 CHUNK_SIZE = 10140
-BACKEND = URL('http://127.0.0.1:5555')
-TIMEOUT = 10
 
 
 @login_required
 async def index(request):
     async with aiohttp.ClientSession() as session:
         session_state = await get_session(request)
+        backend_url = request.app.settings.backend_url
 
-        url = request.url.with_host(BACKEND.host).with_port(BACKEND.port).with_scheme(BACKEND.scheme)
+        url = request.url.with_host(backend_url.host).with_port(backend_url.port).with_scheme(backend_url.scheme)
         headers = request.headers.copy()
-        headers['Host'] = '{}:{}'.format(BACKEND.host, BACKEND.port)
+        headers['Host'] = '{}:{}'.format(backend_url.host, backend_url.port)
         headers['Remote-User'] = session_state['aiohttp_cas']['username']
 
         kwargs = dict(
@@ -32,7 +32,7 @@ async def index(request):
             auth=getattr(request, 'auth', None),
             allow_redirects=False,
             encoding=request.charset,
-            timeout=TIMEOUT,
+            timeout=request.app.settings.timeout,
         )
 
         async with session.request(**kwargs) as remote_response:
@@ -54,10 +54,49 @@ async def index(request):
 
 
 async def make_app():
+    parser = argparse.ArgumentParser(
+        description='Python CAS Gateway',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('--redis-url', help='Redis URL', default='redis://127.0.0.1:6379/0')
+    parser.add_argument('--timeout', type=int, help='Request timeout', default=10)
+    parser.add_argument('--bind-host', help='Address to bind to', default='0.0.0.0')
+    parser.add_argument('--bind-port', type=int, help='Port to bind to', default='8000')
+    parser.add_argument('--cas-version', type=int, help='CAS version in use', default=3)
+    parser.add_argument('--chunk-size', type=int, help='Chunk size for streaming responses back to the client',
+                        default=1024*4)
+
+    required_named = parser.add_argument_group('required named arguments')
+    required_named.add_argument('--backend-url', help='URL to the backend to be proxied (e.g. http://myhost.com:8888)',
+                                required=True)
+    required_named.add_argument('--cas-url', help='URL to the CAS server', required=True)
+
+    args = parser.parse_args()
     app = web.Application()
-    pool = await create_pool(('127.0.0.1', 6379), db=0)
+
+    app.settings = type('Settings', (object,), dict(
+        backend_url=URL(args.backend_url),
+        cas_url=URL(args.cas_url),
+        redis_url=URL(args.redis_url),
+        timeout=args.timeout,
+        bind_host=args.bind_host,
+        bind_port=args.bind_port,
+        cas_version=args.cas_version,
+    ))()
+
+    pool = await create_pool(
+        (app.settings.redis_url.host, app.settings.redis_url.port),
+        db=int((app.settings.redis_url.path or '/0').strip('/'))
+    )
+
     session_setup(app, RedisStorage(pool))
-    cas_setup(app, '127.0.0.1:8000', host_prefix='/cas/', version='3', host_scheme='http')
+    cas_setup(
+        app=app,
+        host=app.settings.cas_url.host,
+        host_prefix=app.settings.cas_url.path,
+        version=app.settings.cas_version,
+        host_scheme=app.settings.cas_url.scheme
+    )
     app.router.add_route('*', '/{tail:.*}', index)
     return app
 
