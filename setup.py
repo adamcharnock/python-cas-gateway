b789ed7572721c10bf1d4411a92eb25093f18c50
Adam Charnock <adam@adamcharnock.com>
2018-07-31 22:52:37 +0100

    Fixing version conflicts

--- setup.py
+++ setup.py
@@ -15,10 +15,11 @@ setup(
     description='Asyncio cas gateway/proxy',
     long_description=open('README.rst').read() if exists("README.rst") else "",
     install_requires=[
-        'aiohttp>=2.3.6,<3',
+        'aiohttp>=3,<3.2',
         'aiohttp-cas>=0.1,<1',
         'aioredis>=1.0.0<2',
         'aiohttp-remotes==0.0.5',
+        'boto3>=1.7.66,<1.8',
     ],
     include_package_data=True,
     entry_points={
